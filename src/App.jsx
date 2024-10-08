3653a1f19c0f3279181cee2927fb9b85e5b32ff3
DukaSiqueira <e.siqueira@edu.unipar.br>
2024-10-07 21:28:34 -0300

    Refactoring code to make it more readable and less verbose

--- src/App.jsx
+++ src/App.jsx
@@ -11,10 +11,18 @@ function App() {
         <section id='core-concepts'>
           <h2>Core Concepts</h2>
           <ul>
+            {/* 
+              Example of hardcoding the CoreConcept components
             <CoreConcept {...CORE_CONCEPTS[0]}/>
             <CoreConcept {...CORE_CONCEPTS[1]}/>
             <CoreConcept {...CORE_CONCEPTS[2]}/>
-            <CoreConcept {...CORE_CONCEPTS[3]}/>
+            <CoreConcept {...CORE_CONCEPTS[3]}/> */}
+            
+            
+            {/* Example of using the map method to dynamically render the CoreConcept components */}
+            {CORE_CONCEPTS.map((concept, index) => (
+              <CoreConcept key={index} {...concept} />
+            ))}
           </ul>
 
         </section>
